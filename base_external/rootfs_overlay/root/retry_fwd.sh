0786eca62d8e8d6c8177e4ab187484326e2d59b3
jbohn3353 <jbohn3353@gmail.com>
2022-12-04 22:29:35 -0700

    fix typos

--- base_external/rootfs_overlay/root/retry_fwd.sh
+++ base_external/rootfs_overlay/root/retry_fwd.sh
@@ -2,13 +2,13 @@
 # called during start in the background to continuosly attempt to forward the ssh
 # port from the remote instance, retrying on closure after a delay
 # Usage:
-#   ./retry_fwd.sh [keypath] [port] [instance_ip]
+#   ./retry_fwd.sh
 # Author:
 #   James Bohn
 
 set -e
 
-retry_delay=30
+retry_delay=5
 
 while : ; do
     echo $(/root/fwd_ssh.sh) # this echo wrapper appears to prevent the scipt
