1c2651d34b92d41d8558fb2e47309335811db857
jbohn3353 <jbohn3353@gmail.com>
2022-12-04 22:22:47 -0700

    Add keep alive packet to target ssh connection (removing some instabilities)

--- base_external/rootfs_overlay/root/fwd_ssh.sh
+++ base_external/rootfs_overlay/root/fwd_ssh.sh
@@ -13,8 +13,8 @@ default_instance_ip="34.224.219.227"
 
 keypath=${1:-${default_keypath}}
 port=${2:-${default_port}} 
-instace_ip=${3:-${default_instance_ip}  }
+instace_ip=${3:-${default_instance_ip}}
 
-ssh -y -N -i ${keypath} -R ${port}:127.0.0.1:22 ec2-user@${instace_ip}
+ssh -K 5 -y -N -i ${keypath} -R ${port}:127.0.0.1:22 ec2-user@${instace_ip}
 
 exit $?
